e20e9cd6e4fbec0c0b50cd38ef6e0eaef85092a0
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-06-21 07:14:34 +0800

    Add files via upload

---
+++ #019/js/main.js
@@ -0,0 +1,17 @@
+// alert('JavaScript is linked!')
+
+
+// This is a single-line comment
+
+/* 
+This a multi-line comment 
+*/
+
+// ctrl + shift + i to open the console
+console.log('This is a \'string\'') // str
+console.log('This is an integer:', 824) // int
+console.log('This is a float:', 0.824) // float
+console.log('These are booleans:', true, false) // boolean
+
+console.log('\nHello World, as always') 
+console.log('25 plus 3 raise to 3 is equals to', 25+3**3) // You can do operations, just like in any other language syempre HAHHAA
