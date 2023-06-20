6af4da0e4055cc5777e23aebf12e9fb17d112da9
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-06-21 07:19:59 +0800

    Update main.js

--- #019/js/main.js
+++ #019/js/main.js
@@ -1,4 +1,4 @@
-// alert('JavaScript is linked!')
+alert('JavaScript is linked!')
 
 
 // This is a single-line comment
@@ -8,10 +8,9 @@ This a multi-line comment
 */
 
 // ctrl + shift + i to open the console
+console.log('Hello World, as always')
 console.log('This is a \'string\'') // str
 console.log('This is an integer:', 824) // int
 console.log('This is a float:', 0.824) // float
 console.log('These are booleans:', true, false) // boolean
-
-console.log('\nHello World, as always') 
-console.log('25 plus 3 raise to 3 is equals to', 25+3**3) // You can do operations, just like in any other language syempre HAHHAA
+console.log('You can also do operations: 25 plus 3 raise to 3 is equals to', 25+3**3) // You can do operations, just like in any other language syempre HAHHAA
