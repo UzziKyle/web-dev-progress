094864d6f32ca3e9c6d5bc5f15d1f5a76ee8ccdb
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-06-28 11:01:09 +0800

    Add files via upload

---
+++ #024/js/main.js
@@ -0,0 +1,15 @@
+// alert('JavaScript is linked!')
+
+// BOM and DOM
+// BOM - Browser Object Model
+// The BOM refers to the set of JavaScript objects browsers provide you with. 
+
+console.log(window) // This will show you all the methods that you can use
+
+window.alert('This is annoying.') // We can use any of the propery methods of the window object.
+// window.propertyName or just propertyName
+alert('I\'m really sorry.')
+
+// DOM - Document Object Model
+// The DOM refers to your HTML in JavaScript. 
+// You can access it by typing in 'document' on the console.
