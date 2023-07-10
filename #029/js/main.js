eacd240479f59b73d6b385cf83a155cc56f4ac7c
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-07-10 19:20:28 +0800

    Delete #029 directory

--- #029/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('button')
-const body = document.body
-
-menuButton.addEventListener('click', e => {
-    menuButton.classList.toggle('menu-is-open')
-    body.classList.toggle('menu-is-open')
-})
